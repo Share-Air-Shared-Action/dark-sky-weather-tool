d36ecda20dc79714c7d4c784bad4d124b85d0a9c
Paula Mendez <paulipotter@gmail.com>
2017-08-18 18:44:08 -0500

    updated constant values

--- constants.py
+++ constants.py
@@ -3,10 +3,11 @@ API_KEY = "3d5abd0dd03dc038cc654a14255173cb"
 LATITUDE = "41.786"
 LONGITUDE = "-87.752"
 
-# April 18th 2017 at 12:30:00am
+# April 18th 2017 at 12:00:00am
 FIRST_DATE = 1492475200
-LAST_DATE = 0
+LAST_DATE = 1502989200 
 DAY = 86400
+
 
 API_LINK = "https://api.darksky.net/forecast/{0}/{1},{2}".format(API_KEY,LATITUDE,LONGITUDE)
 
@@ -22,4 +23,4 @@ HUMIDITY = "humidity"
 PRECIPITATION = "precipIntensity"
 UV_INDEX = "uvIndex"
 
-
+QUERY = "INSERT INTO  darksky-weather VALUES "
