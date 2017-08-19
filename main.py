8f0091bcd7f049470a5be73d50ff54b42870ad11
Paula Cecilia Mendez Sanchez <paulipotter@fingolfin.cs.ksu.edu>
2017-08-18 21:45:14 -0500

    syntax errors fixed

--- main.py
+++ main.py
@@ -5,48 +5,46 @@ import psycopg2
 from json import dump
 
 # Connect to database
-conn = psycopg2.connect("dbname='sasa-airquality' user='postgres'")
+conn = psycopg2.connect("dbname='sasa_airquality' user='postgres'")
 
 cur = conn.cursor()
 
-yesterday = (datetime.now() - timedelta(days=1)).isoformat()
+yesterday = int((datetime.now() - timedelta(days=1)).strftime('%s'))
 # print(data.json())
 
 #First time usage only
 date = FIRST_DATE
 query = QUERY
-while(date <= yesterday):
+while(date <= LAST_DATE):
     #renew API Link to ask for information
-    api_link = "{0},{1}".format(API_LINK,date)
-
+    api_link = "{0},{1}".format(API_LINK,date) 
     #HTTP GET Request that returns a json dict
     result = get(api_link)
     data = result.json()
     hour = 0 
-    val = []
     for hour in range(24):
-        hourly = data[HOURLY][DATA][HOUR] 
-        val.extend([hourly[TIMESTAMP], hourly[TEMP], hourly[PRESSURE]])
-        val.extend([hourly[WIND_SPEED], hourly[WIND_DIRECTION], hourly[HUMIDITY]])
-        val.extend([hourly[PRECIPITATION], hourly[UV_INDEX]])
+        val = []
+        hourly = data[HOURLY][DATA][hour] 
+        val.extend([hourly[TIMESTAMP]])
+
+        for col in COLUMNS:
+            if col in hourly.keys():
+                if col == "humidity":
+                    val.extend([int(hourly[col]*100)])
+                else:
+                    val.extend([hourly[col]])
+            else:
+                val.extend([-1])
+
+  #       print(val, len(val)) 
 
         if date >= LAST_DATE:
-            query += "(val[:]);"
+            query += "({0});".format(val[:])
         else:
-            query+="(val[:]), "
+            query+="({0}), ".format(val[:])
     
     date += DAY
+    
+print(query)
+#cur.execute(query)
 
-cur.execute(query)
-#Loop to create string
-#print(weather) 
-#def first_time():
-##
-#    date = FIRST_DATE
-#    yesterday = (datetime.now() - timedelta(days=1)).isoformat()
-#    weather = forecast(*CHICAGO, time=yesterday)
-#    print(weather) 
-##    while date <= yesterday:
-#        weather = forecast(*CHICAGO, time=yesterday)
-#        print(weather) 
-#        date = date + DAY
